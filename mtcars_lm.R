dfa6290f05e543b6808a9ff7b2b2f98a2ce06e19
rupendrab <rbandyo@yahoo.com>
2015-08-22 20:39:58 -0400

    Added Function multisimulation to mtcars_lm.R

--- mtcars_lm.R
+++ mtcars_lm.R
@@ -56,3 +56,14 @@ lm_mtcars_1 <- function(df, field) {
         allfits <- cbind(d2, d1)
         ret
 }
+
+multisimulation <- function() {
+        models <- lm_mtcars_1(df = mtcars1, field = "am")
+        allfits <- models("allfits")
+        topfits <- allfits %>%
+                filter(maxp < 0.1) %>%
+                mutate(adjrsq = round(adjrsq,2), sigma = round(sigma,2)) %>%
+                arrange(desc(adjrsq), maxp, sigma)
+        topfits <- topfits[1:10,]
+        kable(topfits)
+}
